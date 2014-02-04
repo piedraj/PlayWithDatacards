3891295409a9678f1d38df088cb272d0baa6c9ba
Andrea <massironi.andrea@gmail.com>
2014-02-04 12:05:00 +0100

    upgrade

--- scripts/doDatacard2TableWWewk.sh
+++ scripts/doDatacard2TableWWewk.sh
@@ -40,3 +40,17 @@ python bigTableFromCards.py  scripts/WWewk/qqHWWlnln-WW2jewk-2012-01Jan-Cut2012-
 echo "DF tight tche"
 python bigTableFromCards.py  scripts/WWewk/qqHWWlnln-WW2jewk-2012-01Jan-Cut2012-DF-05/hww-19.36fb.mH125.of_2jtche05_shape.txt    -f scripts/joinSamples.py      -D
 # tableFromCards.py
+
+
+
+
+cp -r /afs/cern.ch/user/a/amassiro/scratch0/VBF/Limit/CMSSW_6_1_0/src/qqHWWlnln-WW2jewk-All-01Jan-Cut2012   scripts/WWewk/
+sed 's/qqHWWlnln-WW2jewk-2012-01Jan-Cut2012/..\/qqHWWlnln-WW2jewk-2012-01Jan-Cut2012/g'  scripts/WWewk/qqHWWlnln-WW2jewk-All-01Jan-Cut2012/hww-19.36fb.mH125.txt   >   tmp.txt
+mv tmp.txt scripts/WWewk/qqHWWlnln-WW2jewk-All-01Jan-Cut2012/hww-19.36fb.mH125.txt
+
+cd scripts/WWewk/qqHWWlnln-WW2jewk-All-01Jan-Cut2012/
+python    ../../../../ModificationDatacards/TransformShapeToCutBased.py     -d    hww-19.36fb.mH125.txt
+cd -
+
+echo "All"
+python tableFromCards.py  scripts/WWewk/qqHWWlnln-WW2jewk-All-01Jan-Cut2012/hww-19.36fb.mH125.txt
--- tableFromCards.py
+++ tableFromCards.py
@@ -55,7 +55,7 @@ for channel in errors:
 
 for x in DC.exp:
     for y in DC.exp[x]:
-        print "%10s %10s %10.2f +/- %10.2f (rel = %10.2f)" % (x,y,DC.exp[x][y],DC.exp[x][y]*errors[x][y],errors[x][y])
+        print "%10s %10s %10.2f +/- %10.2f (%10.2f \\%%)" % (x,y,DC.exp[x][y],DC.exp[x][y]*errors[x][y],errors[x][y]*100)
 
 
 
@@ -78,7 +78,7 @@ print "} \\hline"
 print (" "),
 for channel in DC.exp:
     print ("& %13s " % channel),
-print ("\\\\")
+print ("\\\\ \\hline")
 
 signals     = DC.list_of_signals()
 backgrounds = DC.list_of_backgrounds()
@@ -92,7 +92,7 @@ for s in signals :
     print (" %13s " % s),
     for channel in DC.exp:
         if s in DC.exp[channel].keys(): # possible that some backgrounds appear only in some channels
-            print (" & %10.2f +/- %10.2f (rel = %10.2f) " % (DC.exp[channel][s],DC.exp[channel][s]*errors[channel][s],errors[channel][s])),
+            print (" & %10.2f +/- %10.2f (%10.0f \\%%) " % (DC.exp[channel][s],DC.exp[channel][s]*errors[channel][s],errors[channel][s]*100)),
             if channel not in    totsig.keys():    totsig[channel] = 0.0
             if channel not in errtotsig.keys(): errtotsig[channel] = 0.0
             totsig[channel]    = totsig[channel]    + DC.exp[channel][s]
@@ -105,10 +105,10 @@ for s in signals :
 
 
 print ("\\hline")
-print ("& %13s " % "signal"),
+print (" %13s " % "signal"),
 for channel in DC.exp:
     errtotsig[channel] = sqrt(errtotsig[channel])
-    print (" & %10.2f +/- %10.2f (rel = %10.2f) " % (totsig[channel],errtotsig[channel],errtotsig[channel]/totsig[channel])),
+    print (" & %10.2f +/- %10.2f (%10.0f \\%%) " % (totsig[channel],errtotsig[channel],errtotsig[channel]/totsig[channel]*100)),
 print ("\\\\")
 print ("\\hline")
 
@@ -117,7 +117,7 @@ for b in backgrounds :
     print (" %13s " % b),
     for channel in DC.exp:
         if b in DC.exp[channel].keys(): # possible that some backgrounds appear only in some channels
-            print (" & %10.2f +/- %10.2f (rel = %10.2f) " % (DC.exp[channel][b],DC.exp[channel][b]*errors[channel][b],errors[channel][b])),
+            print (" & %10.2f +/- %10.2f (%10.0f \\%%) " % (DC.exp[channel][b],DC.exp[channel][b]*errors[channel][b],errors[channel][b]*100)),
             if channel not in    totbkg.keys():    totbkg[channel] = 0.0
             if channel not in errtotbkg.keys(): errtotbkg[channel] = 0.0
             totbkg[channel]    = totbkg[channel]    + DC.exp[channel][b]
@@ -132,7 +132,7 @@ print ("\\hline")
 print (" %13s " % "background"),
 for channel in DC.exp:
     errtotbkg[channel] = sqrt(errtotbkg[channel])
-    print (" & %10.2f +/- %10.2f (rel = %10.2f) " % (totbkg[channel],errtotbkg[channel],errtotbkg[channel]/totbkg[channel])),
+    print (" & %10.2f +/- %10.2f (%10.0f\\%%) " % (totbkg[channel],errtotbkg[channel],errtotbkg[channel]/totbkg[channel]*100)),
 print ("\\\\")
 print ("\\hline")
 
@@ -141,6 +141,11 @@ print ("\\hline")
 print "\\end{tabular}"
 print "}"
 print "\\end{center}"
+
+
+print "\n\n\n"
+
+
 print "\\end{table}"
 
 
