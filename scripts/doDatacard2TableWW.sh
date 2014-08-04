50df65bd56ce3bdb6fe340b3ebb88b355a784ce6
Andrea <massironi.andrea@gmail.com>
2014-08-04 14:32:57 +0200

    update

--- scripts/doDatacard2TableWW.sh
+++ scripts/doDatacard2TableWW.sh
@@ -20,8 +20,8 @@ echo "################################"
 echo "-->> big table <<--"
 
 echo "DF 0J"
-python bigTableFromCards.py  /afs/cern.ch/user/a/amassiro/public/xLatinos/ww/WWDFcut0jet/hww-19.36fb.mH125.of_0j_shape.txt
+python bigTableFromCards.py  --file scripts/joinSamples.py   /afs/cern.ch/user/a/amassiro/public/xLatinos/ww/WWDFcut0jet/hww-19.36fb.mH125.of_0j_shape.txt
 
 echo "SF 0J"
-python bigTableFromCards.py  /afs/cern.ch/user/a/amassiro/public/xLatinos/ww/WWSFcut0jet/hww-19.36fb.mH125.sf_0j_shape.txt
+python bigTableFromCards.py  --file scripts/joinSamples.py   /afs/cern.ch/user/a/amassiro/public/xLatinos/ww/WWSFcut0jet/hww-19.36fb.mH125.sf_0j_shape.txt
 
