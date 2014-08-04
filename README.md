72e3df847edfbe93d234e7f15e9d7ab9c4b1e42d
Andrea <massironi.andrea@gmail.com>
2014-08-04 14:18:23 +0200

    update

--- README.md
+++ README.md
@@ -25,6 +25,9 @@ look here:
 # expand all nuisances:
 
     python bigTableFromCards.py  /afs/cern.ch/user/a/amassiro/public/xLatinos/ww/WWDFcut0jet/hww-19.36fb.mH125.of_0j_shape.txt
+    python bigTableFromCards.py --file scripts/joinSamples.py  /afs/cern.ch/user/a/amassiro/public/xLatinos/ww/WWDFcut0jet/hww-19.36fb.mH125.of_0j_shape.txt
+
+
 
 e.g.
 
--- scripts/joinSamples.py
+++ scripts/joinSamples.py
@@ -6,11 +6,11 @@
 # joinSamples.update({'VV'  : ['YY','XX']})
 
 
-joinSamples.update({'Higgs'        : ['qqH','ggH']})
+joinSamples.update({'Higgs'        : ['qqH','ggH','ZH','WH']})
 joinSamples.update({'VVV-WZ-ZZ-Vg' : ['VV','VVV','Vg','VgS']})
 joinSamples.update({'WW'           : ['ggWW','WW']})
 joinSamples.update({'Top'          : ['TopPt0','TopPt1']})
-joinSamples.update({'DY'           : ['DYee','DYmm']})
+joinSamples.update({'DY'           : ['DYee','DYmm','DYTT']})
 
 #joinSamples.update({'all' : ['VV','VVV','Vg','VgS','ggWW','WW','TopPt0','TopPt1','DYee','DYmm']})
 
