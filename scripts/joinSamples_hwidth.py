1985d39ad08285d4dfa3b57c2c71e5921744bb8a
Andrea <massironi.andrea@gmail.com>
2014-11-19 16:47:18 +0100

    fix uncertainties and hwidth

---
+++ scripts/joinSamples_hwidth.py
@@ -0,0 +1,18 @@
+#
+# join samples
+#
+# join YY and XX into VV
+#
+# joinSamples.update({'VV'  : ['YY','XX']})
+
+
+joinSamples.update({'Off-shell-SBI'        : ['ggH_sbi','qqH_sbi']})
+joinSamples.update({'Off-shell-S'          : ['ggH_s','qqH_s']})
+joinSamples.update({'Off-shell-B'          : ['ggH_b','qqH_b']})
+joinSamples.update({'Higgs'        : ['qqH','ggH','ZH','WH']})
+joinSamples.update({'VVV-WZ-ZZ-Vg-DY-WJet' : ['VV','VVV','Vg','VgS','DYTT','WJet']})
+joinSamples.update({'Top'          : ['Top']})
+
+
+#joinSamples.update({'all' : ['VV','VVV','Vg','VgS','ggWW','WW','TopPt0','TopPt1','DYee','DYmm']})
+
