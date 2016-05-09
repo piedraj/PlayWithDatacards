3d0be8971b0bda05c8aa3f4ca326246a84fde793
amassiro <massironi.andrea@gmail.com>
2016-05-09 15:51:51 +0200

    new

---
+++ scripts/joinSamples_ggH.py
@@ -0,0 +1,14 @@
+#
+# join samples
+#
+# join YY and XX into VV
+#
+# joinSamples.update({'VV'  : ['YY','XX']})
+
+
+joinSamples.update({'Higgs'        : [ 'ggZH_hww', 'qqH_hww',  'WH_hww',   'ggH_hww',  'H_htt',  'ZH_hww' ]})
+joinSamples.update({'VVV-WZ-ZZ-Vg' : [ 'VVV','Vg','VZ']})
+#joinSamples.update({'WW'  : ['ggWW','WW']})
+
+#joinSamples.update({'all' : ['VV','VVV','Vg','VgS','ggWW','WW','TopPt0','TopPt1','DYee','DYmm']})
+
